62f3ea1eb3c7ccd68d8e464a388c89ba33a71d5c
Jie Tao <tsquall121@gmail.com>
2023-09-18 00:01:17 -0500

    Add comments

--- driver.bash
+++ driver.bash
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# get all of the daily data from all weather stations and generate list of
+# stations
 code/get_ghcnd_data.bash ghcnd_all.tar.gz
+code/get_ghcnd_all_files.bash
+
+# get list of type of data find at each weather station
 code/get_ghcnd_data.bash ghcnd-inventory.txt
+
+# get metadata for each weather station
 code/get_ghcnd_data.bash ghcnd-stations.txt
