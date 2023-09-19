f6880fd1c98ff5951b80824d8c639f882d7583a1
Jie Tao <tsquall121@gmail.com>
2023-09-18 20:18:56 -0500

    Split files and filter them and read them in using R

--- code/concatenate_dly.bash
+++ code/concatenate_dly.bash
@@ -2,4 +2,8 @@
 
 mkdir -p data/temp
 
-tar Oxvzf data/ghcnd_all.tar.gz | grep "PRCP" | split l-1000000 --filter 'gzip > data/temp/$FILE.gz'
+tar Oxvzf data/ghcnd_all.tar.gz | grep "PRCP" | split -l 1000000 --filter 'gzip >  data/temp/$FILE.gz'
+
+code/read_split_dly_files.R
+
+rm -rf data/temp
